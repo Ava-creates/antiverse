f9a1a9211fb8620ecbccf1218695198b34de316b
Ava-creates <avanitiwari2002@gmail.com>
2022-03-09 21:10:46 +0400

    update

--- js/script.js
+++ js/script.js
@@ -1,21 +1,35 @@
+
+$(window).scroll(function(){
+    var scroll = $(window).scrollTop();
+    $(".zoom img").css({
+        width: (100+scroll/5)+"%"
+    });
+});
+
 const flightPath = {
     curviness: 0.2,
-    autoRotate: true,
+    autoRotate: false,
     values: [
-        { x: 100, y: -30 },
-        { x: 300, y: 90 },
-        { x: 500, y: -30 },
-        { x: 700, y: 90 },
-        { x: 900, y: -30 },
-        { x: 1100, y: 90 },
-        { x: window.innerWidth, y: -30 }
+        { x: 10, y: -70 },
+        { x: 50, y: -50 },
+        { x: 100, y: -43 },
+        { x: 150, y: -45 },
+        { x: 280, y: -47 },
+        { x: 350, y: -50 },
+        { x: 410, y: -40 },
+        { x: 460, y: -35 },
+        { x: 500, y: -40 },
+        { x: 550, y: -50 },
+     
+      
+
     ]
 }
 
 const tween = new TimelineLite();
 
 tween.add(
-    TweenLite.to("#ship2", 1, {
+    TweenLite.to("#ship", 1, {
         bezier: flightPath,
         ease: Power1.easeInOut
     })
@@ -24,36 +38,15 @@ tween.add(
 const Controller = new ScrollMagic.Controller();
 
 const scene = new ScrollMagic.Scene({
-    triggerElement: "#panel-3",
-    duration: 15000,
+    triggerElement: "#panel-2",
+    duration: 5000,
     triggerHook: 0
 })
     .setTween(tween)
     .addIndicators()
-    .setPin('#panel-3')
+    .setPin('#panel-2')
     .addTo(Controller);
 
-/* for displaying text in panel 3 */
-
-var texts=["the earthhhhhh", "it it is flattttt", "aaaaaa"];
-
-
-var d= 0;
-
-if(d<25)
-{
-    $(".texts").text(texts[0])
-}
-
-else if( d>25 && d<50 )
-{
-    $(".texts").text(texts[1])
-}
-
-else
-{
-    $(".texts").text(texts[0])
-}
 
 
 
@@ -67,13 +60,71 @@ function make_visible()
     document.getElementById("splash").style.display="none";
 }
 
+let scrollPositionY = 0;
 
-console.log(Y.scrollTop)
-if(Y.scrollTop==10)
-{
 
-    setTimeout(make_visible(), 3000);
-    console.log(Y.scrollTop)
 
-}
+
+window.addEventListener("scroll", (evt)=>{
+    scrollPositionY = window.scrollY;
+    console.log("scroll position Y: ", scrollPositionY);
+    if(scrollPositionY>800 && scrollPositionY<6000)
+    {
+        $("#ship").css("top","scrollPositionY");
+        if(scrollPositionY<3000)
+        {
+            $("#pop2").css("visibility","visible");
+        }
+
+        else{
+            $("#pop2").css("visibility","hidden");
+        }
+
+        if(3050<scrollPositionY && scrollPositionY<3900)
+        {$("#pop3").css("visibility","visible");}
+
+        else
+        {
+            $("#pop3").css("visibility","hidden");
+        }
+
+        if(4500<scrollPositionY && scrollPositionY<5000)
+        {$("#pop1").css("visibility","visible");}
+
+        else
+       { ("#pop1").css("visibility","hidden");}
+
+        
+    }
+
+    else 
+    {
+        
+        $("#pop3").css("visibility","hidden");
+        $("#pop1").css("visibility","hidden");
+    }
+
+    if(scrollPositionY>7000 )
+    {
+        if(scrollPositionY<7500)
+        {
+
+          
+            $("#ship2").css("top","37rem");
+            $("#ship2").css("left","36rem");
+       
+        }
+    }
+
+    else
+    {
+        $("#ship2").css("top","5rem");
+        $("#ship2").css("left","6rem");
+    }
+    
+
+})
+
+
+
    
